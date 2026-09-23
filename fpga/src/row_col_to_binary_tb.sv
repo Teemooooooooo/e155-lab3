5c97cca308cebda88f76473ab7696983f666982e
Jinghe Yu <101943663+Teemooooooooo@users.noreply.github.com>
2026-09-23 11:26:23 -0700

    udpate with another case

--- fpga/src/row_col_to_binary_tb.sv
+++ fpga/src/row_col_to_binary_tb.sv
@@ -52,7 +52,7 @@ module row_col_to_binary_tb();
 		c_sync = 4'b1110;
         #900000;       
 		assert (key == 16'b10)       // check outputs
-				$display("PASSED! key is at 1");
+				$display("PASSED! key is at 10");
 			else 
 				$error("FAILED! key = %0b",key); 
 				
@@ -65,14 +65,29 @@ module row_col_to_binary_tb();
 		c_sync = 4'b1101;
         #900000;
         assert (key == 16'b0000000000100010)       // check outputs
-				$display("PASSED! key is at 0");
+				$display("PASSED! key is at expected value");
 			else 
 				$error("FAILED! key = %0b",key); 
 				
 		assert (press == 0)       // check outputs
 				$display("PASSED! press = 0");
 			else 
+				$error("FAILED! press = %0b", press);
+				
+		r_sync = 4'b0010;                
+		c_sync = 4'b1111;
+		#900000;
+       assert (key == 16'b10)       // check outputs
+				$display("PASSED! key is at 1");
+			else 
+				$error("FAILED! key = %0b",key); 
+				
+		assert (press == 1'b1)       // check outputs
+				$display("PASSED! press = 1");
+			else 
 				$error("FAILED! press = %0b", press); 
+
+
 
 
     #100 $stop;
